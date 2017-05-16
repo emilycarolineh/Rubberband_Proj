c7d227b4892e201480478efc4f74a497a6a05d26
JustinTsunamictech <Justin@tsunamictech.com>
2017-05-16 14:10:37 -0400

    Update PegBand.java

--- PegBand.java
+++ PegBand.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 public class PegBand
@@ -5,23 +6,30 @@ public class PegBand
     public static void main(String[] args)
     {
         Graph thisGraph = new Graph();
-        improverOne(thisGraph);
+        int[] positionArray = new int[thisGraph.adjacencyList.length];
+
+        for(int i = 0; i < thisGraph.adjacencyList[].length; i++)
+            positionArray[i] = i;
+
+        int originalCost = calculateCost(positionArray, thisGraph.adjacencyList);
+        improverOne(positionArray, thisGraph.adjacencyList);
+        compareCost(originalCost, thisGraph.calculateCost())
         
     }
     
-    public int CalculateCost(int[] position, int[][] adjacencyList)
+    public int calculateCost(int[] position, int[][] adjacencyList)
     {
         //The cost of the permutation in its entirety.
         int totalCost = 0;
 
         //Traverse the entire adjacency list to identify each pair of pegs bounded by a band.
-        for(int i = 1; i < adjacencyList.length; i++)
+        for(int i = 0; i < adjacencyList.length; i++)
         {
-            for(int j = 1; j < adjacencyList[i].length; j++)
+            for(int j = 0; j < adjacencyList[i].length; j++)
             {
                 //Store both pegs in the pair.
                 int firstPeg = adjacencyList[i];
-                Int secondPeg = adjacencyList[i][j];
+                int secondPeg = adjacencyList[i][j];
 
                 //Compare the positions of the pegs. Add the difference between them to the cost 
                 //of the permutation.
@@ -33,8 +41,36 @@ public class PegBand
         }   
      }
      
-     public int[] improverOne(int[] position)
+     public int[] improverOne(int[] position, int[][] adjacencyList)
      {
-        
+        //Tracks number of connections for each post in position array.
+        int[] connectionCount = new int[position.length];
+
+        //Stores post # with highest number of connections.
+        int post = 0;
+
+        for(int i = 0; i < adjacencyList.length; i++)
+        {
+            for(int j = 0; j < adjacencyList[i].length; j++)
+            {
+                connectionCount[i] += adjacencyList[i].length;
+
+                //add other instances of each element to connections count here.............*********
+            }
+        }
+
+        //Stores the highest connection count.
+        int highestConCount = 0;
+
+        for(int i = 0; i < connectionCount.length; i++)
+        {
+            if(connectionCount[i] > highestConCount)
+                highestConCount = i;
+        }
      } 
+
+     public int compareCost(int origCost, int newCost)
+     {
+        return (newCost - origCost); 
+     }
 }
